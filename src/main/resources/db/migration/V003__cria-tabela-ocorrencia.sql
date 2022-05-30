b3d0618597c9e2830200ccb5bc088c15af23f3fd
Edson Sales <edsonsales99@gmail.com>
2022-05-29 20:06:55 -0500

    Criando o recurso para cadastrar uma ocorrencia em uma entrega

---
+++ src/main/resources/db/migration/V003__cria-tabela-ocorrencia.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE ocorrencia(
+	id bigint not null AUTO_INCREMENT,
+    entrega_id bigint not null,
+    descricao text not null,
+    data_registro datetime not null,
+	
+    PRIMARY KEY (id)
+);
+
+ALTER table ocorrencia add CONSTRAINT fk_ocorrencia_entrega
+FOREIGN KEY (entrega_id) REFERENCES entrega (id);
